37c3ed1678b461c45e2c999195cb7431c2eb0521
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-01-21 10:27:06 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,22 @@
+function moveDot(direction,max) {
+  var count = 0
+  while (count < max) {
+    direction
+    count = count + 1
+  }
+}
+
+moveDot(down,8)
+right
+right
+moveDot(up,7)
+right
+right
+moveDot(down,7)
+right
+right
+moveDot(up,7)
+right
+right
+moveDot(down,7)
+right
