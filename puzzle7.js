510dc8f5e530e0becf91df9a6048c68c22a73df3
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-01-21 10:22:27 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,26 @@
+function dotDown {
+  while (getColor != red) {
+    down
+  }
+}
+
+function dotUp {
+  while (getColor != red) {
+    up
+  }
+}
+
+dotDown
+right
+right
+dotUp
+right
+right
+dotDown
+right
+right
+dotUp
+right
+right
+dotDown
+right
