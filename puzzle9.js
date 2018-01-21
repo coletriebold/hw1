027455c1db457d426c1fbb339cf54b5d07ec4f89
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-01-21 10:30:14 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,13 @@
+function colorMoveDot(direction,max) {
+  var count = 0
+  while (count < max) {
+    setColor('black')
+    direction
+    count = count + 1
+  }
+}
+
+colorMoveDot(down,9)
+colorMoveDot(right,9)
+colorMoveDot(up,9)
+colorMoveDot(left,9)
