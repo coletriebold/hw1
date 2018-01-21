dec9f67e686cd3e749c71734343fad5c74e22978
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-01-21 10:18:30 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,26 @@
+right
+
+function move2 {
+  if (getColor == blue) {
+    right
+  }
+  else {
+    left
+  }
+}
+
+
+if (getColor == blue) {
+  down
+  down
+  down
+  move2
+  down
+}
+else {
+  up
+  up
+  up
+  move2
+  down
+}
